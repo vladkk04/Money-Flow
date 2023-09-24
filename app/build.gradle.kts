5a5e6cfafbe86ded86c567bf03dad612c4753c01
Lindgren-Askold <vladuslaw512@gmail.com>
2023-09-24 22:16:05 +0200

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,15 +42,15 @@ android {
 
 dependencies {
     val roomVersion = "2.5.2"
-    val navVersion = "2.6.0"
+    val navVersion = "2.7.3"
 
     implementation ("androidx.room:room-ktx:$roomVersion")
     implementation ("androidx.room:room-runtime:$roomVersion")
     ksp ("androidx.room:room-compiler:$roomVersion")
-    androidTestImplementation ("androidx.room:room-testing:$roomVersion")
+    androidTestImplementation ("androidx.room:room-testing:2.5.2")
 
 
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
